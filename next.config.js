7dfeaaf17c52a039057d9e5b616fb2a47d013070
Simple134 <sanchezjosue2001@gmail.com>
2024-11-15 08:31:22 -0400

    some changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['api.gestiono.app'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.gestiono.app',
+        pathname: '/**',
+      },
+    ],
   },
 }
 
